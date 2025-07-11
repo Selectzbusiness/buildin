e6a5f27da3b6f4bd3b5207884f6d57e2e94498a9
Selectz <Selectz.business@gmail.com>
2025-07-11 05:43:28 +0530

    Name changed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jobconnect.app',
-  appName: 'Job Connect',
+  appId: 'com.selectz.app',
+  appName: 'Selectz',
   webDir: 'build'
 };
 
